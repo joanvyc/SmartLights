8298ca371668af9c8c6ba9b578e1c1e908a5122d
Andrea Querol <andrea.querol@bsc.es>
2019-11-24 09:33:36 +0100

    arduino entering in mode deepsleep

--- sensor_management/sensor_management.ino
+++ sensor_management/sensor_management.ino
@@ -10,13 +10,63 @@ const char* password = "0123456789";
 const char * host = "192.168.1.118";
 const uint16_t port = 3000;
 
-int light_values [LIGHT_SIZE];
-int light_counter = 0;
-int flow_counter = 0;
+struct {
+    uint32_t crc32;
+    int light_counter;
+    int light_values [LIGHT_SIZE];
+    int flow_counter;
+} data;
+/*
+   int light_values [LIGHT_SIZE];
+   int light_counter = 0;
+   int flow_counter = 0;
+ */
+uint32_t calculateCRC32(const uint8_t *data, size_t length) {
+    uint32_t crc = 0xffffffff;
+    while (length--) {
+        uint8_t c = *data++;
+        for (uint32_t i = 0x80; i > 0; i >>= 1) {
+            bool bit = crc & 0x80000000;
+            if (c & i) {
+                bit = !bit;
+            }
+            crc <<= 1;
+            if (bit) {
+                crc ^= 0x04c11db7;
+            }
+        }
+    }
+    return crc;
+}
 
-void setup () {
-    //Setup serial for log
-    Serial.begin(115200);
+//prints all rtcData, including the leading crc32
+void printMemory() {
+    char buf[3];
+    uint8_t *ptr = (uint8_t *)&data;
+    for (size_t i = 0; i < sizeof(data); i++) {
+        sprintf(buf, "%02X", ptr[i]);
+        Serial.print(buf);
+        if ((i + 1) % 32 == 0) {
+            Serial.println();
+        } else {
+            Serial.print(" ");
+        }
+    }
+    Serial.println();
+}
+
+
+String json_parser () {
+
+    String json_data = String("{ \"flow_label\": ") + data.flow_counter + ", \"samples\": [ " + data.light_values[0];
+    for (int i = 1 ; i < LIGHT_SIZE; i++) json_data += String(", ") + data.light_values[i];
+    json_data += "] } \0";
+    Serial.println( "JSON");
+    Serial.println(json_data);
+    return json_data;
+}
+
+void init_wifi() {
     Serial.println();
     Serial.println();
 
@@ -38,19 +88,11 @@ void setup () {
     Serial.println("WiFi connnected");
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
-
-    // LDR
-    for (int i = 0; i < LIGHT_SIZE; i++) light_values[i] = 0;
-}
-
-
-bool getPetition(WiFiClient &cl,  String path){
-    cl.print("GET " + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n" + "\r\n");
 }
 
 bool post_petition (WiFiClient &cl,  String path, String data) {
+    Serial.println("POST");
     cl.print("POST " + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Accept: application/json\r\n" + "Content-Type: application/json\r\n" + "Content-Length: " + data.length() + "\r\n" + "Connection: close\r\n" + "\r\n" + data + "\r\n");
-    //cl.print("POST " + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Content-Type: application/json\r\n" + "Content-Lenght: " + data.length() + "\r\n" + "Connection: close\r\n" + "\r\n" + data + "\r\n");
 }
 
 void send_data (String data) {
@@ -65,68 +107,63 @@ void send_data (String data) {
         post_petition(cl, "/api/postData", data);
     }
 }
-/*
-   bool send_key (String key){
-   WiFiClient cl;
-   if (!cl.connect(host, port)) {
-   Serial.println("Connection failed");
-   }
 
-   if (cl.connected()) {
-   put_petition(cl, "/api/user_enter", key);
-//        getPetition(cl, "/api/user_enter");
-Serial.println("Connection sended ");
-Serial.print(key);
-return true;
-}
-return false;
-}
+void setup() {
+    Serial.begin(115200);
+    Serial.setTimeout(2000);
+    // Read struct from RTC memory
+    if (ESP.rtcUserMemoryRead(0, (uint32_t*) &data, sizeof(data))) {
+        Serial.println("Read: ");
+        printMemory();
+        Serial.println();
+        uint32_t crcOfData = calculateCRC32((uint8_t*) &data.light_counter, sizeof(data) - sizeof(data.crc32));
+        Serial.print("CRC32 of data: ");
+        Serial.println(crcOfData, HEX);
+        Serial.print("CRC32 read from RTC: ");
+        Serial.println(data.crc32, HEX);
+        if (crcOfData != data.crc32) {
+            Serial.println("CRC32 in RTC memory doesn't match CRC32 of data. Data is probably invalid!");
+            data.crc32 = 0;
+            data.light_counter = 0;
+            for (int i = 0; i < LIGHT_SIZE; i++) data.light_values[i] = 0;
+            data.flow_counter = 0;
+        } else {
+            Serial.println("CRC32 check ok, data is probably valid.");
+        }
+    }
 
- */
-void delay_s(int s){
-    for(int i = 0; i<s; i++) delay(1000);
-}
+    // Generate new data set for the struct
+    int tmp = analogRead(LDR_PIN);
+    data.light_values[data.light_counter] = tmp;
+    data.light_counter++;
+    Serial.println(tmp);
+    Serial.println(data.light_counter);
 
-String json_parser () {
-    
-    String json_data = String("{ \"flow_label\": ") + flow_counter + ", \"samples\": [ " + light_values[0];
-    for (int i = 1 ; i < LIGHT_SIZE; i++) json_data += String(", ") + light_values[i];
-    json_data += "] } \0";
-    Serial.println( "JSON");
-    Serial.println(json_data);
-    return json_data;
-}
+    if (data.light_counter == LIGHT_SIZE) {
+        init_wifi();
+        send_data(json_parser());
+        data.light_counter = 0;
+        data.flow_counter++;
+        delay(1000);
+    }
 
+    data.crc32 = calculateCRC32((uint8_t*) &data.light_counter, sizeof(data) - sizeof(data.crc32));
+
+    // Write struct to RTC memory
+    if (ESP.rtcUserMemoryWrite(0, (uint32_t*) &data, sizeof(data))) {
+        Serial.println("Write: ");
+        printMemory();
+        Serial.println();
+    }
+
+
+    // Deep sleep mode for 30 seconds, the ESP8266 wakes up by itself when GPIO 16 (D0 in NodeMCU board) is connected to the RESET pin
+    Serial.println("I'm awake, but I'm going into deep sleep mode for 30 seconds");
+    ESP.deepSleep(4e6);
+
+}
 
 void loop() {
-    /*
-       if(doorOpen()){
-       door_time = millis();
-
-       if (in_house){
-    //user was in house -> exiting
-    sendOutHouse();
-    }else{
-    //user was outside -> is entering
-    sendInHouse();
-    }
-    }
-     */
-
-    int tmp = analogRead(LDR_PIN);
-    Serial.println(tmp);
-    light_values[light_counter] = tmp; 
-
-    delay(1000);
-    if (light_counter == LIGHT_SIZE-1) {
-        String json_data = json_parser();
-        flow_counter++;
-        send_data(json_data);
-    }
-
-    //counter = (++counter)%LIGHT_SIZE;
-    Serial.println(light_counter);
-    light_counter = (++light_counter)%LIGHT_SIZE;
 
 }
 
